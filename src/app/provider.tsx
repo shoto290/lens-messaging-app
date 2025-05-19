73161c10e038599d5eabe0a9eb4ae6abcbc5820e
Shoto <38501963+shoto290@users.noreply.github.com>
2025-05-19 11:03:21 +0200

    fix: wallet connect

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 import { ReactNode } from "react";
 import { ThemeProvider } from "next-themes";
+import { env } from "@/env";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,8 +23,7 @@ const config = createConfig(
     transports: {
       [chains.mainnet.id]: http(chains.mainnet.rpcUrls.default.http[0]!),
     },
-    walletConnectProjectId:
-      process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
+    walletConnectProjectId: env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
     appName: "Lens Messaging App",
     appDescription: "A messaging app built on Lens Protocol",
     appUrl: "https://lens-messaging-app.vercel.app",
